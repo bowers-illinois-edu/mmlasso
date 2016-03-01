6dde33aef5fc8c2dfd4f5c2b7300011a6193fc84
Jake Bowers <jake@jakebowers.org>
2016-03-01 17:29:16 -0600

    Working to add adaptive ridge functionality using rr_se_vec and parallizing the adaptive part

--- R/sridge.R
+++ R/sridge.R
@@ -30,7 +30,7 @@ sridge <- function(x, y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0, denormou
     }
 
     # Spherical Principal Components (no centering) privar,  Beig= vector of robust
-    #'eigenvalues' and matrix of eigenvectors Xnor is now = PCA scores
+    # "eigenvalues" and matrix of eigenvectors Xnor is now = PCA scores
     pca <- SPCC(Xnor)
     privar <- pca$lamda
     Beig <- pca$b
@@ -209,7 +209,7 @@ const_marina <- function(delta){
 #' @export
 sridge2 <- function(x,  y,  cualcv.S = 5,  nkeep = 5,  numlam.S = 30,  niter.S = 50,  normin = 0,
                     denormout = 0,  alone = 0,  ncores = 1,  lamdas = NULL,  deltas = NULL,  adaptive = FALSE) {
-    #  Solves n*s_n^2 +lam*||beta1||^2} = min. Adapted from Ricardo Maronna's original
+    #  Solves n*s_n^2 +lam*||beta1||^2 = min. Adapted from Ricardo Maronna's original
     #  MATLAB code. INPUT cualcv.S: method for estimating prediction error.
     #  cualcv-fold cross-validation nkeep: number of candidates to be kept for full
     #  iteration in the Pena-Yohai procedure (default=5) normin: normalize input
@@ -238,7 +238,7 @@ sridge2 <- function(x,  y,  cualcv.S = 5,  nkeep = 5,  numlam.S = 30,  niter.S =
     }
 
     #  Spherical Principal Components (no centering) privar,  Beig= vector of robust
-    #  'eigenvalues' and matrix of eigenvectors Xnor is now = PCA scores
+    #  "eigenvalues" and matrix of eigenvectors Xnor is now = PCA scores
     pca <- SPCC(Xnor)
     privar <- pca$lamda
     Beig <- pca$b
@@ -254,90 +254,97 @@ sridge2 <- function(x,  y,  cualcv.S = 5,  nkeep = 5,  numlam.S = 30,  niter.S =
         deltas <- 0.5 * (1 - (pp)/n)  # for the M-scale used with Penia-Yohai
     }
 
-    resmaker <- function( Xnor,  ynor,  nkeep,  niter.S,  Beig){ ##  ,  mux, adaptive){
-        force(Xnor); force(ynor); force(nkeep); force(niter.S); force(Beig); force(adaptive) ##  ; force(mux)
-        function(lamda, delta){
-            fin <- rr_se(X = Xnor,  y = ynor,  lambda2 = lamda,
-                         deltaesc = delta,  cc_scale = 1,  nkeep,  niter.S,  epsilon = 1e-04)
-            beta <- fin$coef
-            betaslo <- beta[2:(length(beta))]
-            bint <- beta[1]
-            res <- ynor - Xnor %*% betaslo - as.vector(bint)
-            edf <- fin$edf
-            deltult <- 0.5 * (1 - (edf + 1)/n)  ##  'delta' for final scale
-            deltult <- max(c(deltult,  0.25))
-            #  c0: constant for consistency of final scale
-            c0 <- const_marina(deltult)
-            sigma <- Mscale_mar(res,  deltult,  c0)
-            a_cor <- mean(psi_marina(res/sigma,  c0)^2)
-            b_cor <- mean(Mchi(res/sigma,  c0,  "bisquare",  2))
-            c_cor <- mean(psi_marina(res/sigma,  c0) * (res/sigma))
-            corr <- 1 + (edf + 1)/(2 * n) * (a_cor/(b_cor * c_cor))
-            fscale <- sigma * corr  # bias correction for final scale
-            #  Back from PC to ordinary coordinates
-            betaslo <- Beig %*% betaslo
-            if(adaptive){
-                activ <- which(abs(beta[2:length(beta)]) > .Machine$double.eps)
-                ###Take out covariates not chosen by MMLasso and scale the remaining ones
-                w.ad <- abs(beta[2:length(beta)][activ])
-                Xnor.w <- as.matrix(scale(Xnor[, activ], center=FALSE,  scale = 1/w.ad))
-                ###Calculate candidate lambdas for adaptive MM-ridge
-                lamda.ad <- genlambdas(Xnor.w, ynor, numlam.S=length(lamdas))
-                fin.ad <- rr_se(X = Xnor.w,  y = ynor,  lambda2 = lamda.ad[, "lamda"],
-                                deltaesc = lamda.ad[, "delta"],
-                                cc_scale = 1,  nkeep,  niter.S,  epsilon = 1e-04)
-                beta.ad <- fin.ad$coef
-                betaslo.ad <- rep(0,p)
-                betaslo.ad[activ] <- beta.ad[2:(length(beta.ad))] * w.ad
-                betaslo.ad <- betaslo.ad ## for now, staying very close to code in mmlasso.R
-                bint.ad <- beta.ad[1]
-                res.ad <- ynor - Xnor.w %*% betaslo.ad - as.vector(bint)
-                edf.ad <- fin.ad$edf
-                deltult.ad <- 0.5 * (1 - (edf.ad + 1)/n)  ##  'delta' for final scale
-                deltult.ad <- max(c(deltult.ad,  0.25))
-                #  c0: constant for consistency of final scale
-                c0.ad <- const_marina(deltult.ad)
-                sigma.ad <- Mscale_mar(res.ad,  deltult.ad,  c0.ad)
-                a_cor.ad <- mean(psi_marina(res.ad/sigma.ad,  c0.ad)^2)
-                b_cor.ad <- mean(Mchi(res.ad/sigma.ad,  c0.ad,  "bisquare",  2))
-                c_cor.ad <- mean(psi_marina(res.ad/sigma.ad,  c0.ad) * (res.ad/sigma.ad))
-                corr.ad <- 1 + (edf.ad + 1)/(2 * n) * (a_cor.ad/(b_cor.ad * c_cor.ad))
-                fscale.ad <- sigma.ad * corr.ad  # bias correction for final scale
-                #  Back from PC to ordinary coordinates
-                betaslo.ad <- Beig %*% betaslo.ad
-                bint <- bint.ad
-                betaslo <- betaslo.ad
-                fscale <- fscale.ad
-                edf <- edf.ad
-            } else {
-                lamda.ad <- t(c(lamda=NA, delta=NA))
-            }
-
-            ##  De-normalize beta if required by user
-            ##  if (normin == 1 & denormout == 1) {
-            ##      betaslo <- betaslo/sigx
-            ##      bint <- muy + bint - mux %*% betaslo
-            ##  }
-            beta <- c(bint,  betaslo)
-            re <- list(coef = beta,  scale = fscale,  edf = edf,  lamda = lamdas,  delta = deltas, 
-                       lamda.ad = lamda.ad[,"lamda"], delta.ad = lamda.ad[,"delta"])
-            return(re)
-        }
+    fin <- rr_se_vec(X = Xnor,  y = ynor,  lambda2 = lamdas,
+                     deltaesc = deltas,  cc_scale = 1,  nkeep,  niter.S,  epsilon = 1e-04)
+    betas <- fin$coef
+    res <- apply(betas,2,function(b){
+                     ynor - Xnor %*% b[-1] - as.vector(b[1])
+                     })
+    edf <- fin$edf
+    deltult <- 0.5 * (1 - (edf + 1)/n)  ##  'delta' for final scale
+    ## deltult <- max(c(deltult,  0.25))
+    ## deltult <- pmax(deltult,  rep(0.25,length(deltult)))
+    deltult <- deltult * (deltult > .25) + .25 * ( 1 - (deltult > .25) )
+    #  c0: constant for consistency of final scale
+    getfscale<-function(thedeltult,theres,theedf){
+        c0 <- const_marina(thedeltult)
+        sigma <- Mscale_mar(theres,  thedeltult,  c0)
+        a_cor <- mean(psi_marina(theres/sigma,  c0)^2)
+        b_cor <- mean(Mchi(theres/sigma,  c0,  "bisquare",  2))
+        c_cor <- mean(psi_marina(theres/sigma,  c0) * (theres/sigma))
+        corr <- 1 + (theedf + 1)/(2 * n) * (a_cor/(b_cor * c_cor))
+        fscale <- sigma * corr  # bias correction for final scale
+        return(fscale)
     }
+    fscale <- sapply(1:ncol(res),function(i){
+                         getfscale(thedeltult=deltult[i],
+                                   theres=res[,i],theedf=edf[i]) })
+    #  Back from PC to ordinary coordinates
+    betaslo <- apply(betas[-1,],2,function(b){ Beig %*% b})
 
-    getresults <- resmaker(Xnor,  ynor,  nkeep,  niter.S,  Beig) ##  ,  mux)
-
-    if(ncores > 1){
-        ##  assuming name of cluster is cl for now
-        clusterExport(cl, "getresults", envir=environment())
-        results <- parLapply(cl, 1:length(lamdas),  function(i){
-                                 getresults(lamda=lamdas[[i]], delta=deltas[[i]])
-                                })
-    } else {
-        results <- lapply(1:length(lamdas), function(i){
-                              getresults(lamda=lamdas[[i]], delta=deltas[[i]])
-                                })
+    if(!adaptive){
+        lamdaad <- t(as.matrix(c(lamda=NA,delta=NA)))
+        betares <- rbind(betas[1,],  betaslo)
+        results <- list(coef = betares,  scale = fscale,  edf = edf,  lamda = lamdas,  delta = deltas,
+                        lamdaad = lamdaad[,"lamda"], deltaad = lamdaad[,"delta"])
         return(results)
+    } else {
+        resmaker <- function( Xnor,  ynor,  nkeep,  niter.S,  Beigi, numlam.S){
+            force(Xnor); force(ynor); force(nkeep); force(niter.S); force(Beig); force(numlam.S) ##  ; force(mux)
+            function(thelamda, thedelta, thebetas){
+                activ <- which(abs(thebetas[2:length(thebetas)]) > .Machine$double.eps)
+                ###Take out covariates not chosen by MMLasso and scale the remaining ones
+                wad <- abs(thebetas[2:length(thebetas)][activ])
+                Xnorw <- as.matrix(scale(Xnor[, activ], center=FALSE,  scale = 1/wad))
+                ###Calculate candidate lambdas for adaptive MM-ridge
+                lamdaad <- genlambdas(Xnorw, ynor, numlam.S=numlam.S)
+                lamdaad <- lamdaad[lamdaad[,"lamda"]!=0,]
+                fin <- rr_se_vec(X = Xnorw,  y = ynor,  lambda2 = lamdaad[,"lamda",drop=FALSE],
+                                 deltaesc = lamdaad[,"delta",drop=FALSE],  cc_scale = 1,  nkeep,  niter.S,  epsilon = 1e-04)
+                betas <- fin$coef
+                betasbig <- matrix(0,ncol=p,nrow=nrow(fin$coef))
+                betasbig[,activ] <- fin$coef
+                betasbig[-1,] <- betasbig[-1,]*wad
+                res <- apply(betasbig,2,function(b){
+                                 ynor - Xnorw %*% b[-1] - as.vector(b[1])
+                                 })
+                edfbig<-rep(0,p)
+                edfbig[activ] <- fin$edf
+                deltult <- 0.5 * (1 - (edfbig + 1)/n)  ##  'delta' for final scale
+                deltult <- deltult * (deltult > .25) + .25 * ( 1 - (deltult > .25) )
+                fscale <- sapply(1:ncol(res),function(i){
+                                     getfscale(thedeltult=deltult[i],
+                                               theres=res[,i],theedf=edf[i]) })
+                #  Back from PC to ordinary coordinates
+                betasload <- apply(betasbig[-1,],2,function(b){ Beig %*% b})
+                beta <- rbind(betas[1,],  betasload)
+                results <- list(coef = betasload,  scale = fscale,  edf = edf,  lamda = thelamda,  delta = thedelta,
+                                lamdaad = lamdaad[,"lamda"], deltaad = lamdaad[,"delta"])
+                return(results)
+
+                ##  De-normalize beta if required by user
+                ##  if (normin == 1 & denormout == 1) {
+                ##      betaslo <- betaslo/sigx
+                ##      bint <- muy + bint - mux %*% betaslo
+                ##  }
+            }
+        }
+
+        getresults <- resmaker(Xnor,  ynor,  nkeep,  niter.S,  Beig, numlam.S)
+
+        if(ncores > 1){
+            ##  assuming name of cluster is cl for now
+            clusterExport(cl, c("getresults","getfscale"), envir=environment())
+            results <- parLapply(cl, 1:ncol(betas),  function(i){
+                                     getresults(thelamda=lamdas[[i]], thedelta=deltas[[i]], thebetas=betas[,i])
+                                })
+        } else {
+            results <- lapply(1:ncol(betas), function(i){
+                                  message(i)
+                                  getresults(thelamda=lamdas[[i]], thedelta=deltas[[i]], thebetas=betas[,i])
+                                })
+        }
+
     }
 }
 
